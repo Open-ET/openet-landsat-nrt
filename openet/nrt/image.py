a083904e9811c63110d2f1d67822bd7f0ac2f681
Charles Morton <cgmorton@users.noreply.github.com>
2024-07-21 18:06:42 -0700

    Use QA_PIXEL as base image to keep original image geometry

--- openet/nrt/image.py
+++ openet/nrt/image.py
@@ -106,12 +106,13 @@ def landsat8(landsat_toa):
         # 'system:id': landsat_toa.get('system:id'),
     }
 
+    # Start with the QA_PIXEL Bands to keep the original image geometry
     # Scale (unscale?) the reflectance and LST to match a LANDSAT/LC08/C02/T1_L2 image
     # Setting output type to match T1_L2 image types
     output_img = (
-        landsat_rt_sr.subtract(-0.2).divide(0.0000275).round().clamp(0, 65535).uint16()
-        .addBands([landsat_lst.subtract(149.0).divide(0.00341802).round().clamp(0, 65535).uint16()])
-        .addBands(landsat_toa.select(['QA_PIXEL', 'QA_RADSAT']))
+        landsat_toa.select(['QA_PIXEL', 'QA_RADSAT'])
+        .addBands(landsat_rt_sr.subtract(-0.2).divide(0.0000275).round().clamp(0, 65535).uint16())
+        .addBands(landsat_lst.subtract(149.0).divide(0.00341802).round().clamp(0, 65535).uint16())
         .set(properties)
     )
 
